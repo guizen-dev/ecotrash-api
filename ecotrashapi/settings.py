74cc2573fa488f3c21ded693a7805defe92cb077
Joelson Campos <guilherme.campos3931@gmail.com>
2023-11-22 00:12:02 -0300

    allow all

--- ecotrashapi/settings.py
+++ ecotrashapi/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG =  os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
-ALLOWED_HOSTS += os.environ.get("ALLOWED_HOSTS").split()
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
